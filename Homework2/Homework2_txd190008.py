555a202255f18cecb5fcc3ac211dc4f05052bfce
TediDika <72334852+TediDika@users.noreply.github.com>
2023-02-17 16:42:10 -0600

    Added comments

--- Homework2/Homework2_txd190008.py
+++ Homework2/Homework2_txd190008.py
@@ -11,8 +11,6 @@ from random import randint
 seed(1234)
 
 
-
-
 def preprocess(raw_text):
     # Uses a list comprehension to remove non alphas and make lower case
     tokens = [t.lower() for t in word_tokenize(raw_text) if t.isalpha()]
@@ -22,7 +20,7 @@ def preprocess(raw_text):
     wnl = WordNetLemmatizer()
     lemmatized = [wnl.lemmatize(t) for t in removedStops]
     uniqueLemmas = set(lemmatized)
-    # POS Tagging
+    # POS Tagging, uses regex to find POS that start with N to filter for nouns
     tags = nltk.pos_tag(uniqueLemmas)
     print("\nFirst 20 tagged words: ", tags[:20])
     nounLemmas = [token for token, pos in tags if re.match(r'^N', pos)]
@@ -33,26 +31,24 @@ def preprocess(raw_text):
     return removedStops, nounLemmas
 
 
-
-
-
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Error: Please enter a filename as a system arg')
         quit()
 
+    # OS file reading compatibility
     rel_path = sys.argv[1]
     with open(pathlib.Path.cwd().joinpath(rel_path), 'r') as f:
         text_in = f.read()
 
-    # Calculate Lexical Diversity
+    # Calculate Lexical Diversity, # of unique tokens/total # of tokens
     tokenizedText = word_tokenize(text_in)
     set1 = set(tokenizedText)
     print("\nLexical diversity: %.2f" % (len(set1) / len(tokenizedText)))
 
     partATokens, nouns = preprocess(text_in)
 
-    # Make a dictionary
+    # Make a dictionary of {noun:count of noun in tokens} and print 50 most common words
     counts = {t:partATokens.count(t) for t in nouns}
     sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
     print("\n50 most common words:")
@@ -61,23 +57,26 @@ if __name__ == '__main__':
         print(sorted_counts[i])
         listOfWords.append(sorted_counts[i][0])
 
-
-    # Guessing Game #
+    # Guessing Game
     print("\nLets play a word guessing game!")
     TotalPoints = 5
     while True:
         RoundPoints = 5
-        randomWord = listOfWords[randint(0, 49)]
         printList = []
+        # Picks random word from top 50
+        # Print list will be the length of the random word and filled with "_"
+        randomWord = listOfWords[randint(0, 49)]
         for i in randomWord:
             printList.append("_")
             print('_', end=" ")
         print("\nForsenCD:", randomWord)
         while True:
             guess = input("\nGuess a letter:")
+            # Game ends when "!" is input
             if guess == '!':
                 print("\nGame Over!")
                 quit()
+            # When they guess letter correctly, add that letter to the print list
             if guess in randomWord:
                 RoundPoints += 1
                 print("Right! Score is", RoundPoints)
@@ -85,6 +84,8 @@ if __name__ == '__main__':
                     if randomWord[i] == guess:
                         printList[i] = guess
             else:
+                # When they guess incorrectly, decrement Round points
+                # Round ends when Round points hits negative, Total points decrements
                 RoundPoints -= 1
                 if RoundPoints < 0:
                     TotalPoints -= 1
@@ -93,22 +94,18 @@ if __name__ == '__main__':
                     break
                 else:
                     print("Sorry, guess again. Score is", RoundPoints)
+            # Prints letters guessed correctly so far
             for i in printList:
-                print(i, end =" ")
+                print(i, end=" ")
+            # Round ends when they have guessed all letters, Round points is added to Total points
             if "_" not in printList:
                 print("\nYou solved it!")
                 TotalPoints += RoundPoints
                 print("\nCurrent score:", TotalPoints)
-                RoundPoints = -1
                 break
-
+        # Game ends when Total points hits negative
         if TotalPoints < 0:
             break
         else:
             print("\nGuess another word")
     print("\nGame Over!")
-
-
-
-
-
