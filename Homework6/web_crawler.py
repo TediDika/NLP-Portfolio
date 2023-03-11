85c294acb1719ed53d7bb61be0698a5a343c597f
TediDika <txd190008@utdallas.edu>
2023-03-11 17:54:19 -0600

    Add overview and comments

--- Homework6/web_crawler.py
+++ Homework6/web_crawler.py
@@ -1,34 +1,29 @@
-import pathlib
+# Tedi Dika
+# txd190008
 
+import pathlib
 import nltk
 import requests
 import re
 from bs4 import BeautifulSoup
 from nltk import word_tokenize, sent_tokenize
-import os
 from nltk.corpus import stopwords
-import string
-import math
-from nltk.stem import WordNetLemmatizer
 import pickle
-from pathlib import Path
 
-'''
-Starts with a starter page and extracts all links from that page and puts them in a queue. 
-Then goes to each link in the queue and scrapes all links and adds them to the queue
-'''
+
+# Crawls through starter page and extracts max of 15 links
 def web_crawl():
     starter_url = 'https://en.wikipedia.org/wiki/Attack_on_Titan_(TV_series)'
     r = requests.get(starter_url)
     data = r.text
     soup = BeautifulSoup(data, 'html.parser')
 
-    # write urls to a file
+    # Puts all found urls into list
     url_list = []
     for link in soup.find_all('a'):
         if len(url_list) > 14:
             break
-
+        # Filters links for relevant terms
         link_str = str(link.get('href'))
         if 'Titan' in link_str or 'titan' in link_str and 'manga' not in link_str and 'archive' not in link_str:
             if link_str.startswith('/url?q='):
@@ -46,35 +41,31 @@ def web_crawl():
     print("end of crawler")
     return url_list
 
-
+# Scrapes text off of provided links and then stores them into text files
 def scrape_page(urls):
-
     counter = 1
     for url in urls:
-        current_page = requests.get(url)
-        current_file = open(('text' + str(counter) + '.txt'), 'w', encoding="utf-8")
-
-        current_soup = BeautifulSoup(current_page.content, 'html.parser')
+        page = requests.get(url)
+        file = open(('text' + str(counter) + '.txt'), 'w', encoding="utf-8")
+        soup = BeautifulSoup(page.content, 'html.parser')
 
         # kill all script and style elements
-        for script in current_soup(["script", "style"]):
+        for script in soup(["script", "style"]):
             script.extract()  # rip it out
 
-        for p in current_soup.select('p'):
-            current_file.write(p.get_text() + '\n')
-
-
+        for p in soup.select('p'):
+            file.write(p.get_text() + '\n')
 
         counter += 1
     print("end of scraper")
 
-def clean_text(urls):
 
+# Reads in previously created text files, cleans them up, and outputs new text files
+def clean_text(urls):
     counter = 1
     for url in urls:
         with open(('text' + str(counter) + '.txt'), 'r', encoding="utf-8") as page:
             text = page.read()
-
 
         # Delete newlines and tabs first
         no_tabs = re.sub('\s+', ' ', text)
@@ -88,9 +79,8 @@ def clean_text(urls):
 
     print("end of cleaner")
 
-
+# Output top 25 words based on tf, then create a knowledge base manually
 def important_terms(urls):
-
     # Combine text from all cleaned files
     combined_text = ""
     counter = 1
@@ -100,7 +90,7 @@ def important_terms(urls):
         combined_text += text
         counter += 1
 
-    # extract all tokens, make text lowercase and remove non alphas and stop words
+    # Extract all tokens, make text lowercase and remove non alphas and stop words
     tokens = word_tokenize(combined_text)
     tokens = [t.lower() for t in word_tokenize(combined_text) if t.isalpha()]
     stop_words = stopwords.words('english')
@@ -111,11 +101,11 @@ def important_terms(urls):
     token_set = set(tokens)
     tf_dict = {t: tokens.count(t) for t in token_set}
 
-    # normalize tf by number of tokens
+    # Normalize tf by number of tokens
     for t in tf_dict.keys():
         tf_dict[t] = tf_dict[t] / len(tokens)
 
-    # sort by highest occurring words and print
+    # Sort by highest occurring words and print
     sorted_tf = sorted(tf_dict.items(), key=lambda x: x[1], reverse=True)
     print('\nTop 25 terms using tf:')
     for i in range(25):
@@ -126,19 +116,14 @@ def important_terms(urls):
               'isayama': [], 'eren': [], 'television': []}
 
     sentences = sent_tokenize(combined_text.lower())
-
+    # Iterate through every sentence in the text extracted from the hyperlinks.
+    # If the word was in the sentence, add that sentence to the respective term’s array in the dictionary.
     for sentence in sentences:
         for key, value in top_10.items():
             if key in sentence:
                 value.append(sentence)
 
-    '''
-    # Print the knowledge base
-    for key in top_10:
-        print(key + ':')
-        for sentence in top_10[key]:
-            print('\t' + sentence)
-'''
+    # Print out knowledge base
     print("\nFirst five sentences contained for each term in the Knowledge Base:\n")
     for key, value in top_10.items():
         print(key + ":")
@@ -155,6 +140,3 @@ if __name__ == '__main__':
     scrape_page(url_list)
     clean_text(url_list)
     important_terms(url_list)
-
-
-
